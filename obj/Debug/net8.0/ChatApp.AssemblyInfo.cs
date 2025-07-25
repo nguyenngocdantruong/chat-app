6121e116e19f99165144c60a2d8f448a4261d9c8
Nguyen Dan Truong <145260800+nguyenngocdantruong@users.noreply.github.com>
2025-07-25 12:49:46 +0700

    ..

--- obj/Debug/net8.0/ChatApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/ChatApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eab8d3cc46d9fe74e8ebead0fd30e23d8b6fc43e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
